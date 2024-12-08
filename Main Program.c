3f319ea39824bd972f84610f442cdc47f63188b8
hanifbambang <hanifbambang30@gmail.com>
2024-12-08 19:42:22 +0700

    Add Structure for user

--- Main Program.c
+++ Main Program.c
@@ -2,12 +2,36 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct
+typedef enum Role{DOCTOR, PATIENT, ADMIN}Role;
+
+typedef struct Doctor
 {
     char DocName[50];
-    char PatientName[50];
-    enum {DOCTOR, PATIENT, ADMIN}Role;
-} user;
+    char DocID[12];
+    struct Patient
+    {
+        char Name[50];
+        char ID[12];
+        char Symptoms[50];
+        char Medicine[50];
+    }Patient;
+}Doctor;
+
+typedef struct Admin
+{
+    struct Medicine
+    {
+        char Name[50];
+        char ID[12];
+    } Medicine;
+    struct User
+    {
+        char Name[50];
+        char ID[12];
+        Role Role[12];
+    }User;
+    
+}Admin;
 
 int main ()
 {
