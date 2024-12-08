743fb71d3c60feb18fe59ba2beafcb45680136b7
hanifbambang <hanifbambang30@gmail.com>
2024-12-08 22:06:25 +0700

    Update 1.11

--- Main Program.c
+++ Main Program.c
@@ -4,20 +4,20 @@
 
 typedef enum Role{DOCTOR, PATIENT, ADMIN}Role;
 
-typedef struct Doctor
+typedef struct DOCTOR
 {
     char DocName[50];
     char DocID[13];
-    struct Patient
+    struct PATIENT
     {
         char Name[50];
         char ID[13];
         char Symptoms[50];
         char Medicine[50];
-    }Patient;
-}Doctor;
+    }PATIENT;
+}DOCTOR;
 
-typedef struct Admin
+typedef struct ADMIN
 {
     struct Medicine
     {
@@ -33,14 +33,17 @@ typedef struct Admin
         Role role;
     }User;
     
-}Admin;
+}ADMIN;
 
 int main ()
 {
+    ADMIN Admin;
     int chose;
+    char type[50];
     char Role[13];
     char username[50];
     char password[50];
+    ADMIN* admins = (ADMIN*)malloc(1 * sizeof(ADMIN));
     printf("Login as: ");
     scanf("%s", &Role);
     if (strcmp(Role, "ADMIN") == 0)
@@ -52,13 +55,26 @@ int main ()
         if ((strcmp(username, "admin") == 0) && (strcmp(password, "01234") == 0))
         {
             printf("Successfully logged in!\n");
-            printf("1. Modify Storage\n");
-            printf("2. Modify Account\n");
+            printf("1. Storage\n");
+            printf("2. Account\n");
             printf("Chose (1-2): ");
             scanf("%d", &chose);
             switch (chose)
             {
             case 1:
+                printf("1. Add\n");
+                printf("2. Modify\n");
+                printf("Chose (1-2): ");
+                scanf("%d", &chose);
+                switch (chose)
+                {
+                    case 1:
+                        printf("Account Type: ");
+                        scanf("%s", &type);
+                        break;
+                }
+                break;
+            case 2:
                 break;
             default:
                 break;
