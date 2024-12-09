ad20501f028147bb4ea77674026cd82573d143d7
hanifbambang <hanifbambang30@gmail.com>
2024-12-09 18:06:55 +0700

    Update 1.1.2

--- Main Program.c
+++ Main Program.c
@@ -4,46 +4,49 @@
 
 typedef enum Role{DOCTOR, PATIENT, ADMIN}Role;
 
-typedef struct DOCTOR
+typedef struct DOCTORS
 {
     char DocName[50];
     char DocID[13];
-    struct PATIENT
-    {
-        char Name[50];
-        char ID[13];
-        char Symptoms[50];
-        char Medicine[50];
-    }PATIENT;
-}DOCTOR;
+    char PatientID[50];
+}DOCTORS;
 
-typedef struct ADMIN
+typedef struct PATIENTS
 {
-    struct Medicine
+    char DocID[13];
+    char Name[50];
+    char ID[13];
+    char Symptoms[50];
+    char Medicine[50];
+}PATIENTS;
+
+typedef struct MEDICINES
+{
+    char Name[50];
+    char ID[13];
+    int stock;
+} MEDICINES;
+
+typedef struct USERS
     {
-        char Name[50];
-        char ID[13];
-    } Medicine;
-    struct User
-    {
-        char Username[50];
-        char Password[50];
-        char Name[50];
-        char ID[13];
-        Role role;
-    }User;
-    
-}ADMIN;
+    char Username[50];
+    char Password[50];
+    char Name[50];
+    char ID[13];
+    enum Role role;
+}USERS;
 
 int main ()
 {
-    ADMIN Admin;
-    int chose;
-    char type[50];
+    int chose, UserKe = 0, DoctorKe = 0, PatientKe = 0, MedicineKe = 0;
+    char type[13];
     char Role[13];
     char username[50];
     char password[50];
-    ADMIN* admins = (ADMIN*)malloc(1 * sizeof(ADMIN));
+    USERS* user = (USERS*)malloc(1 * sizeof(USERS));
+    MEDICINES* mendicine = (MEDICINES*)malloc(1 * sizeof(MEDICINES));
+    PATIENTS* patient = (PATIENTS*)malloc(1 * sizeof(PATIENTS));
+    DOCTORS* doctor = (DOCTORS*)malloc(1 * sizeof(DOCTORS));
     printf("Login as: ");
     scanf("%s", &Role);
     if (strcmp(Role, "ADMIN") == 0)
@@ -68,9 +71,12 @@ int main ()
                 scanf("%d", &chose);
                 switch (chose)
                 {
-                    case 1:
+                    case 2:
                         printf("Account Type: ");
                         scanf("%s", &type);
+                        (strcmp(type, "DOCTOR") == 0) ? user[UserKe].role = DOCTOR : 0;
+                        (strcmp(type, "PATIENT") == 0) ? user[UserKe].role = PATIENT : 0;
+                        printf("");
                         break;
                 }
                 break;
@@ -85,5 +91,9 @@ int main ()
             printf("Test %s", username);
         }
     }
+    free(user);
+    free(patient);
+    free(doctor);
+    free(mendicine);
     return 0;
 }
